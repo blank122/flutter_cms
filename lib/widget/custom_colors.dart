43a98e5bf4b5ff72a38e0c8d5082e71cad255059
Joshua De Vera <deverajoshua02@gmail.com>
2024-11-12 14:41:46 +0800

    added a custom colors (bg colors) to be used on the create themes

---
+++ lib/widget/custom_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primary = Color(0xFF007bff); // .bg-primary
+  static const Color secondary = Color(0xFF6c757d); // .bg-secondary
+  static const Color success = Color(0xFF28a745); // .bg-success
+  static const Color danger = Color(0xFFdc3545); // .bg-danger
+  static const Color warning = Color(0xFFffc107); // .bg-warning
+  static const Color info = Color(0xFF17a2b8); // .bg-info
+  static const Color light = Color(0xFFf8f9fa); // .bg-light
+  static const Color dark = Color(0xFF343a40); // .bg-dark
+  static const Color white = Color(0xFFFFFFFF); // .bg-white
+}
