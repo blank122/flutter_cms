1cc0d9805c2825b63034c7fc08b3dc63daf507a4
Joshua De Vera <deverajoshua02@gmail.com>
2024-11-17 21:07:39 +0800

    added a tenary operator to activate and deactivate the status in the database based on the certain variable dynamically

--- lib/services/change_themes.dart
+++ lib/services/change_themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cms/screens/home.dart';
 import 'package:flutter_cms/services/db/database_helper.dart';
 import 'package:flutter_cms/widget/reusable_snackbar.dart';
 import 'package:intl/intl.dart';
@@ -150,7 +151,10 @@ class ChangeThemes {
               onPressed: () {
                 //function to activate the theme
                 // onActivate();
-                updateStatusTheme(status, themeID, context);
+                status == 1
+                    ? deactivateStatusTheme(status, themeID, context)
+                    : updateStatusTheme(status, themeID, context);
+
                 Navigator.of(context).pop(); // Dismiss the dialog
               },
               child: Text(
@@ -180,13 +184,49 @@ Future<void> updateStatusTheme(
     // Save to SQLite
     developer.log('data to be update saved: $themeID, $status, $updatedDate');
 
-    final int result =
-        await DatabaseHelper().useTheme(themeID, status, updatedDate);
+    final int result = await DatabaseHelper().useTheme(themeID, 1, updatedDate);
 
     developer.log('Survey saved to SQLite with ID: $result');
 
     // Show success Snackbar
     if (context.mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
+      ReusableSnackbar.showSuccessSnackbar(
+          context: context,
+          description: "$themeID has been saved successfully");
+    }
+  } catch (e) {
+    print('Error occurred: $e');
+    if (context.mounted) {
+      ReusableSnackbar.showErrorSnackbar(
+          context: context, description: "Something went wrong: $e");
+    }
+  }
+}
+
+Future<void> deactivateStatusTheme(
+    int status, int themeID, BuildContext context) async {
+  try {
+    // Convert survey data to JSON string
+    DateTime now = DateTime.now();
+    String updatedDate = DateFormat('MM/dd/yyyy HH:mm:ss').format(now);
+
+    // Save to SQLite
+    developer.log('data to be update saved: $themeID, $status, $updatedDate');
+
+    final int result = await DatabaseHelper().useTheme(themeID, 0, updatedDate);
+
+    developer.log('Survey saved to SQLite with ID: $result');
+
+    // Show success Snackbar
+    if (context.mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
       ReusableSnackbar.showSuccessSnackbar(
           context: context,
           description: "$themeID has been saved successfully");
